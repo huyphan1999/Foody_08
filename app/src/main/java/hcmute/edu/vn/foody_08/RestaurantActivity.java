2014a93d332a0b575dbe4142b65f72c07211d6c2
HuyThang <huythangnguyen1999@gmail.com>
2020-06-21 15:50:23 +0700

    Add_Silde3,Update_dialogMessage

--- app/src/main/java/hcmute/edu/vn/foody_08/RestaurantActivity.java
+++ app/src/main/java/hcmute/edu/vn/foody_08/RestaurantActivity.java
@@ -1,12 +1,13 @@
 package hcmute.edu.vn.foody_08;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
+
+import androidx.fragment.app.FragmentActivity;
 
 public class RestaurantActivity extends FragmentActivity {
     TextView txtAddWifi;
@@ -16,13 +17,29 @@ public class RestaurantActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurent);
 
-        txtAddWifi=findViewById(R.id.txtAddWifi);
+        txtAddWifi = findViewById(R.id.txtAddWifi);
         txtAddWifi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RestaurantActivity.this, LoginWifiActivity.class);
-                startActivity(intent);
+                DialogLogin();
             }
         });
+    }
+
+    private void DialogLogin() {
+        final Dialog dialog = new Dialog(this, R.style.Theme_AppCompat_DayNight_DarkActionBar);
+        dialog.setContentView(R.layout.activity_login_wifi);
+        dialog.show();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int displayWidth = displayMetrics.widthPixels;
+        int displayHeight = displayMetrics.heightPixels;
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.copyFrom(dialog.getWindow().getAttributes());
+        int dialogWindowWidth = (int) (displayWidth * 0.9f);
+        int dialogWindowHeight = (int) (displayHeight * 0.35f);
+        layoutParams.width = dialogWindowWidth;
+        layoutParams.height = dialogWindowHeight;
+        dialog.getWindow().setAttributes(layoutParams);
     }
 }
